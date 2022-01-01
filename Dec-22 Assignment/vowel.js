7f8ccbe954b290e5cbb52e6b72d5286100e775a8
lasya2206 <94067773+lasya2206@users.noreply.github.com>
2022-01-01 17:46:57 +0530

    Add files via upload

---
+++ Dec-22 Assignment/vowel.js
@@ -0,0 +1,24 @@
+var char = prompt("enter a alphabet");
+if(char == "a")
+{alert("it is vowel");
+}
+else if ("char == e")
+{
+    alert("it is a vowel");
+}
+else if("char == i")
+{
+    alert("it is a vowel");
+}
+else if("char == o")
+{
+    alert("It is a vowel");
+}
+else if("char == u")
+{
+    alert("It is a vowel");
+}
+else
+{
+    alert("It is a consonant");
+}
