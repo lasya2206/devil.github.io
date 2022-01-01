3030a5098c216101537f01e42f44d3a55bb65f4c
lasya2206 <94067773+lasya2206@users.noreply.github.com>
2022-01-01 18:54:29 +0530

    updated 22 dec assignment

---
+++ Dec-22 Assignment/maximum.js
@@ -0,0 +1,23 @@
+var i =prompt("Enter 1st Number");
+	i=parseInt(i)
+	arr.push(i);
+
+                var i =prompt("Enter 2nd Number");
+	i=parseInt(i)
+	arr.push(i);
+
+               var i =prompt("Enter 3rd Number");
+	i=parseInt(i)
+	arr.push(i);
+    if(num[2]>=num[3] && num[2]>=num[4])
+	{
+	alert("Maximum Number is: " +num[2]);
+	}
+	else if(num[3]>=num[2] && num[3]>=num[4])
+	{
+	alert("Maximum Number is: " +num[3]);
+	}
+	else
+	{
+	alert("Maximum Number is: " +num[4]);
+	} 
