2c041e1100faca1efc59c48ce1c43bdb8d673f18
lasya2206 <94067773+lasya2206@users.noreply.github.com>
2022-01-01 16:50:44 +0530

    Add files via upload

---
+++ Dec-20 Assignment/greatest number.js
@@ -0,0 +1,26 @@
+var abcd = "a,b,c,d"
+if (a>b){
+console.log("a");
+}
+else {
+    console.log("b");
+}
+if (b>c){
+    console.log("b");
+}
+else{
+    console.log("c");
+}
+if(a>c){
+    console.log("a");
+}
+else{
+    console.log("c");
+}
+if(b>c){
+    console.log("b");
+}
+else{
+    console.log("c");
+}
+
---
+++ Dec-20 Assignment/index.js
@@ -0,0 +1,8 @@
+ num = 9;
+if (num>10){
+console.log("No.is greater than 10;")
+}
+else
+{
+console.log("No.is lesser than 10;")
+}
