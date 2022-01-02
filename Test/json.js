0d268dcfd17ecb5632471ea93391e73b2e1ec950
lasya2206 <94067773+lasya2206@users.noreply.github.com>
2022-01-02 18:03:44 +0530

    Test related

---
+++ Test/json.js
@@ -0,0 +1,10 @@
+var jsonData={}
+    var form= document.getElementsByTagName("document");
+    let getFormData=()=>{
+        event.preventDefault() 
+        let formData= new FormData(document[0])
+            
+            formData.forEach(function(value, key);
+            console.log(JSON.stringify(jsonData)
+            alert ("Json Data Printed in The Console Please Check The Console")
+    }
